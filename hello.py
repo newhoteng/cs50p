8188c492146beafb0148379634365ef167dd84bb
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-04 20:38:52 +0000

    Multi-line comment

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
+# docs.python.org/3/library/functions.html
+
 # Ask user for their name
 name = input("What's your name? ")
 
+"""
+Multi-line comment
+Just see
+"""
+
 # Say hello to user
-print("hello,")
-print(name)
+# print("hello, " + name)
+print("hello,", name)
