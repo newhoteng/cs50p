622efdda2ff135deac72a09e9eca64dd25546305
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-05 14:46:35 +0000

    Return values

--- calculator.py
+++ calculator.py
@@ -1,13 +1,22 @@
 # x = int(input("What's x? "))
 # y = int(input("What's y? "))
 
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
 
 # round(number, ndigits=None)
 # z = round(x + y)
 # z = round(x / y, 2)
-z = x / y
+# z = x / y
 
 # print(f"{z:,}")
-print(f"{z:.2f}")
+# print(f"{z:.2f}")
+
+def main():
+  x = int(input("What's x? "))
+  print("x squared is", square(x))
+
+def square(n):
+  return n * n
+
+main()
--- hello.py
+++ hello.py
@@ -2,30 +2,15 @@
 # docs.python.org/3/library/functions.html#round
 # docs.python.org/3/library/stdtypes.html#string-methods
 
-# Ask user for their name
-name = input("What's your name? ").strip().title()
-
-"""
-Multi-line comment
-Just see
-"""
-
-# Remove whitespace from str and capitalize user's name
-# name = name.strip().title()
-
-# Capitalize user's name
-# name = name.capitalize()
-# name = name.title()
+def main():
+  # Ask user for their name
+  name = input("What's your name? ").strip().title()
+  hello(name)
 
 
-# Split user's name into first name and last name
-first, last = name.split(" ")
-
-# Say hello to user
-# print("hello, " + name)
-# print("hello,", name)
-print(f"hello, {first}")
+def hello(to="world"):
+  print("hello,", to)
 
 
-
+main()
 
