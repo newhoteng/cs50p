9a3ebc4273c6fce14bfd2b9eaf8f8fb0a136fbae
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-05 12:58:36 +0000

    Capitalize and title

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 # docs.python.org/3/library/functions.html
+# docs.python.org/3/library/stdtypes.html#string-methods
 
 # Ask user for their name
 name = input("What's your name? ")
@@ -8,7 +9,16 @@ Multi-line comment
 Just see
 """
 
+# Remove whitespace from str
+name = name.strip()
+
+# Capitalize user's name
+# name = name.capitalize()
+name = name.title()
+
 # Say hello to user
 # print("hello, " + name)
 # print("hello,", name)
 print(f"hello, {name}")
+
+
