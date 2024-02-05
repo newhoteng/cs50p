4cc2e3b576d23353177c5f028b9236ae69377ab5
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-05 13:12:09 +0000

    Split

--- hello.py
+++ hello.py
@@ -2,23 +2,29 @@
 # docs.python.org/3/library/stdtypes.html#string-methods
 
 # Ask user for their name
-name = input("What's your name? ")
+name = input("What's your name? ").strip().title()
 
 """
 Multi-line comment
 Just see
 """
 
-# Remove whitespace from str
-name = name.strip()
+# Remove whitespace from str and capitalize user's name
+# name = name.strip().title()
 
 # Capitalize user's name
 # name = name.capitalize()
-name = name.title()
+# name = name.title()
+
+
+# Split user's name into first name and last name
+first, last = name.split(" ")
 
 # Say hello to user
 # print("hello, " + name)
 # print("hello,", name)
-print(f"hello, {name}")
+print(f"hello, {first}")
+
+
 
 
