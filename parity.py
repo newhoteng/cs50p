5e93b813c18321335611b31f9caec883ea63c3c2
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-07 12:56:19 +0000

    Match

--- parity.py
+++ parity.py
@@ -6,10 +6,14 @@ def main():
   else:
     print("Odd")
 
+# def is_even(n):
+#   if n % 2 == 0:
+#     return True
+#   else:
+#     return False
+
 def is_even(n):
-  if n % 2 == 0:
-    return True
-  else:
-    return False
+  # return True if n % 2 == 0 else False
+  return n % 2 == 0
 
 main()
