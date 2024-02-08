8854c2f0e6357868c603503d5eceead6b66830bc
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-08 12:21:21 +0000

    While loop

--- cat.py
+++ cat.py
@@ -1,3 +1,15 @@
-print("meow")
-print("meow")
-print("meow")
+# print("meow")
+# print("meow")
+# print("meow")
+
+# while
+# i = 3
+# while i != 0:
+#   print("meow")
+#   i = i - 1
+
+i = 0
+while i < 3:
+  print("meow")
+  # i = i + 1
+  i += 1
