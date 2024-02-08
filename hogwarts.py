09666caa17fed448b3c1caeab422c957a48de219
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-08 13:27:43 +0000

    List of dictionaries

--- hogwarts.py
+++ hogwarts.py
@@ -22,6 +22,16 @@ students = {
 # print(students["Harry"])
 # print(students["Ron"])
 
-# for key in dictionary
+# for key in dict
+# for student in students:
+#   print(student, students[student], sep=", ")
+
+students = [
+  {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+  {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+  {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel Terrier"},
+  {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
+
 for student in students:
-  print(student, students[student], sep=", ")
+  print(student["name"], student["house"], student["patronus"], sep=", ")
