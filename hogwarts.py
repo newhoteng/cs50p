7abb9899acdf89fa7d531861ce18685eae922d64
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-08 13:14:35 +0000

    Dictionaries

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,4 @@
-students = ["Hermione", "Harry", "Ron"]
+students = ["Hermione", "Harry", "Ron", "Draco"]
 
 # print(students[0])
 # print(students[1])
@@ -7,6 +7,21 @@ students = ["Hermione", "Harry", "Ron"]
 # for student in students:
 #   print(student)
 
-for i in range(len(students)):
-  print(i + 1, students[i])
+# for i in range(len(students)):
+#   print(i + 1, students[i])
 
+
+students = {
+  "Hermione": "Gryffindor",
+  "Harry": "Gryffindor",
+  "Ron": "Gryffindor",
+  "Draco": "Slytherin",
+}
+
+# print(students["Hermione"])
+# print(students["Harry"])
+# print(students["Ron"])
+
+# for key in dictionary
+for student in students:
+  print(student, students[student], sep=", ")
