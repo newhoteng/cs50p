1783205da8375e728bd01c12b3f461669d2bfd5f
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-09 21:33:15 +0000

    Try except

--- number.py
+++ number.py
@@ -1,1 +1,10 @@
-x = int(input("What's x? "))
+
+# x = int(input("What's x? "))
+# print(f"x is {x}")
+
+# To take car of a user typing a string for example
+try:
+  x = int(input("What's x? "))
+  print(f"x is {x}")
+except ValueError:
+  print("x is not an integer")
