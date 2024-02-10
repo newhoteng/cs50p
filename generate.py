e90a34e4e8a7ec8c57f0b8362f74d0aa27cc73c7
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-10 21:06:00 +0000

    Statistics module

--- generate.py
+++ generate.py
@@ -12,7 +12,7 @@ import random
 # number = random.randint(1, 10)
 # print(number)
 
-cards = ["jack", "queen", "king"]
-random.shuffle(cards)
-for card in cards:
-  print(card)
+# cards = ["jack", "queen", "king"]
+# random.shuffle(cards)
+# for card in cards:
+#   print(card)
