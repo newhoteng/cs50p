7ea8577b45390ee0cdbb5902c5ab134ffb95c488
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-10 21:00:32 +0000

    Using functions in random module

--- generate.py
+++ generate.py
@@ -1,10 +1,18 @@
 """docs.python.org/3/library/random.html"""
 # Imports all functions in the random module
 import random
-coin = random.choice(["heads", "tails"])
+# coin = random.choice(["heads", "tails"])
 
 # Other way of using functions in modules
 # from random import choice
 # coin = choice(["heads", "tails"])
 
-print(coin)
+# print(coin)
+
+# number = random.randint(1, 10)
+# print(number)
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+  print(card)
