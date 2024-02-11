a5597df1153b743fa87dddbc648ac7cdb6725f92
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-11 12:05:36 +0000

    Fetching data from an API

---
+++ itunes.py
@@ -0,0 +1,28 @@
+
+"""
+The request package allows you to use APIs in python
+pypi.org/project/requests
+"""
+
+import requests
+import sys
+import json
+
+# if len(sys.argv) != 2:
+#   sys.exit()
+
+# response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+# print(json.dumps(response.json(), indent=2))
+
+"""
+docs.python.org/3/library/json.html
+"""
+
+if len(sys.argv) != 2:
+  sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=5&term=" + sys.argv[1])
+o = response.json()
+for result in o["results"]:
+  print(result["trackName"])
+
