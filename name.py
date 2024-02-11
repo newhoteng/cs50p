30b27d80fd5176efc3ef788041b37c9e83a4b29a
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-11 10:12:45 +0000

    sys.exit

--- name.py
+++ name.py
@@ -8,9 +8,28 @@ import sys
 #   print("Too few arguments")
 
 """Other way - using conditionals to provide useful feedback on errors to user"""
+# if len(sys.argv) < 2:
+#   print("Too few arguments")
+# elif len(sys.argv) > 2:
+#   print("Too many arguments")
+# else:
+#   print("hello, my name is", sys.argv[1])
+
+"""
+Separate error handling from the actual code you care about
+This is buggy bcos program will still proceed to print and raise error when no arg is found at index 1
+"""
+# if len(sys.argv) < 2:
+#   print("Too few arguments")
+# elif len(sys.argv) > 2:
+#   print("Too many arguments")
+
+# print("hello, my name is", sys.argv[1])
+
+"""An option to exit program when wrong number of argv is provided"""
 if len(sys.argv) < 2:
-  print("Too few arguments")
+  sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-  print("Too many arguments")
-else:
-  print("hello, my name is", sys.argv[1])
+  sys.exit("Too many arguments")
+
+print("hello, my name is", sys.argv[1])
