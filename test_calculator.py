04cf460918962b892b28b4760378d5bdad0c76db
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-12 20:41:54 +0000

    Using pytest

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,8 @@
 from calculator import square
 # import calculator
 
-def main():
-  test_square()
+# def main():
+#   test_square()
 
 
 # def test_square():
@@ -10,26 +10,46 @@ def main():
 #     print("2 squared was not 4")
 #   if square(3) != 9:
 #     print("3 squared was not 9")
+  
+"""This is too much code than is desirable"""
+# def test_square():
+#   try:
+#     assert square(2) == 4
+#   except AssertionError:
+#     print("2 squared was not 4")
+#   try:
+#     assert square(3) == 9
+#   except AssertionError:
+#     print("3 squared was not 9")
+#   try:
+#     assert square(-2) == 4
+#   except AssertionError:
+#     print("-2 squared was not 4")
+#   try:
+#     assert square(0) == 0
+#   except AssertionError:
+#     print("0 squared was not 0")
 
-def test_square():
-  try:
-    assert square(2) == 4
-  except AssertionError:
-    print("2 squared was not 4")
-  try:
-    assert square(3) == 9
-  except AssertionError:
-    print("3 squared was not 9")
-  try:
-    assert square(-2) == 4
-  except AssertionError:
-    print("-2 squared was not 4")
-  try:
-    assert square(0) == 0
-  except AssertionError:
-    print("0 squared was not 0")
+"""
+Using pytest
+docs.pytest.org
+"""
+# def test_square():
+#   assert square(2) == 4
+#   assert square(3) == 9
+#   assert square(-2) == 4
+#   assert square(-3) == 9
+#   assert square(0) == 0
+
+def test_positive():
+  assert square(2) == 4
+  assert square(3) == 9
+def test_negative():
+  assert square(-2) == 4
+  assert square(-3) == 9
+def test_zero():
+  assert square(0) == 0
 
 
-
-if __name__ == "__main__":
-  main()
+# if __name__ == "__main__":
+#   main()
