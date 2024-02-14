528c6f1d7ba42fc0f0fd306b3755b5f9c0f0e4a5
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-14 11:34:28 +0000

    Opening a file

---
+++ names.py
@@ -0,0 +1,28 @@
+# names = []
+
+# for _ in range(3):
+#   # name = input("What's your name? ")
+#   # names.append(name)
+#   names.append(input("What's your name? "))
+
+# for name in sorted(names):
+#   print(f"hello, {name}")
+
+"""
+Saving to file
+docs.python.org/3/library/functions.html#open
+"""
+
+"""write overides the contents of the page"""
+# name = input("What's your name? ")
+
+# file = open("names.txt", "w")
+# file.write(name)
+# file.close()
+
+
+name = input("What's your name? ")
+
+file = open("names.txt", "a")
+file.write(name)
+file.close()
