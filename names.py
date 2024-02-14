4c97894fcf971a0f79f984abf50ff7bb04f8a609
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-14 18:07:06 +0000

    Sorting data before printing

--- names.py
+++ names.py
@@ -9,6 +9,7 @@
 #   print(f"hello, {name}")
 
 """
+WRITING TO FILES
 Saving to file
 docs.python.org/3/library/functions.html#open
 """
@@ -21,12 +22,40 @@ docs.python.org/3/library/functions.html#open
 # file.close()
 
 
-name = input("What's your name? ")
+# name = input("What's your name? ")
 
 # file = open("names.txt", "a")
 # file.write(f"{name}\n")
 # file.close()
 
 """This syntax automatically closes the file"""
-with open("names.txt", "a") as file:
-  file.write(f"{name}\n")
+# with open("names.txt", "a") as file:
+#   file.write(f"{name}\n")
+
+"""READING FROM FILES"""
+# with open("names.txt", "r") as file:
+#   lines = file.readlines()
+
+# for line in lines:
+#   print("hello,", line, end="")
+
+"""compacting opening and printing at the same time"""
+# with open("names.txt", "r") as file:
+#   for line in file:
+#     print("hello,", line.rstrip())
+
+"""sorting before printing"""
+"""Option 1"""
+names = []
+
+with open("names.txt") as file:
+  for line in file:
+    names.append(line.rstrip())
+
+for name in sorted(names):
+  print(f"hello, {name}")
+
+"""Option 2"""
+# with open("names.txt") as file:
+#   for line in sorted(file):
+#     print("hello,", line.rstrip())
