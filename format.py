22570c34675d80724922a2ed1a0cb310700d1cd7
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-23 22:00:30 +0000

    Using the walrus operator

--- format.py
+++ format.py
@@ -6,9 +6,14 @@ import re
 #   name = f"{first} {last}"
 # print(f"hello, {name}")
 
+# name = input("What's your name? ").strip()
+# matches = re.search(r"^(.+), *(.+)$", name)
+# if matches:
+#   name = f"{matches.group(2)} {matches.group(1)}"
+# print(f"hello, {name}")
+
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), (.+)$", name)
-if matches:
-  last, first = matches.groups()
-  name = f"{first} {last}"
+# walrus operator
+if matches := re.search(r"^(.+), *(.+)$", name):
+  name = f"{matches.group(2)} {matches.group(1)}"
 print(f"hello, {name}")
