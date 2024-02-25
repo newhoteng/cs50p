8470182bf597005bd2c610702aee5ad495962a36
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-25 23:25:42 +0000

    Using a non-capturing group

--- twitter.py
+++ twitter.py
@@ -9,5 +9,18 @@ url = input("URL: ").strip()
 
 """re.sub(pattern, repl, string, count=0, flags=0)"""
 
-username = re.sub(r"https://twitter.com", "", url)
-print(f"Username: {username}")
+# username = re.sub(r"^(https?://)?(www\.)?twitter\.com", "", url)
+# print(f"Username: {username}")
+
+# matches = re.search(r"^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+
+# if matches:
+#   print(f"Username:", matches.group(2))
+
+"""Using the walrus operator"""
+# if matches := re.search(r"^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+#   print(f"Username:", matches.group(2))
+
+"""Using a non-capturing group"""
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+  print(f"Username:", matches.group(1))
