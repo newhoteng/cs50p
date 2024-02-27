08ab7618121b5a72e4317c90e6f60f7484511c1d
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-27 11:10:58 +0000

    Returning a tuple

---
+++ student.py
@@ -0,0 +1,49 @@
+"""Aproach 1"""
+# name = input("Name: ")
+# house = input("House: ")
+# print(f"{name} from {house}")
+
+"""Aproach 2"""
+# def main():
+#   name = get_name()
+#   house = get_house()
+#   print(f"{name} from {house}")
+
+# def get_name():
+#   return input("Name: ")
+
+# def get_house():
+#   return input("House: ")
+
+# if __name__ == "__main__":
+#   main()
+
+"""Aproach 3"""
+# def main():
+#   name, house = get_student()
+#   print(f"{name} from {house}")
+
+# def get_student():
+#   name = input("Name: ")
+#   house = input("House: ")
+#   """Returns a tupple"""
+#   # return name, house
+#   return (name, house)
+
+# if __name__ == "__main__":
+#   main()
+
+"""Aproach 4"""
+def main():
+  student = get_student()
+  print(f"{student[0]} from {student[1]}")
+
+def get_student():
+  name = input("Name: ")
+  house = input("House: ")
+  """Returns a tupple"""
+  # return name, house
+  return (name, house)
+
+if __name__ == "__main__":
+  main()
