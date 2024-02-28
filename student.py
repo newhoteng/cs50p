dba43b23593d403d5df2eeed89e5aa5a28b28796
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-28 18:54:08 +0000

    Returning a dictionary

--- student.py
+++ student.py
@@ -34,16 +34,33 @@
 #   main()
 
 """Aproach 4"""
+# def main():
+#   student = get_student()
+#   print(f"{student[0]} from {student[1]}")
+
+# def get_student():
+#   name = input("Name: ")
+#   house = input("House: ")
+#   """
+#   Returns a tupple, but you can return a list eg return [name, house]
+#   tuples are immutable
+#   """
+#   # return name, house
+#   return (name, house)
+
+# if __name__ == "__main__":
+#   main()
+
+"""Aproach 4"""
 def main():
   student = get_student()
-  print(f"{student[0]} from {student[1]}")
+  print(f"{student['name']} from {student['house']}")
 
 def get_student():
-  name = input("Name: ")
-  house = input("House: ")
-  """Returns a tupple"""
-  # return name, house
-  return (name, house)
+  student = {}
+  student["name"] = input("Name: ")
+  student["house"] = input("House: ")
+  return student
 
 if __name__ == "__main__":
   main()
