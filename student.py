16d2b9cb719b1775796248b243af2eff8e68a616
Harriet Oteng <harriet.oteng@gmail.com>
2024-02-29 15:13:17 +0000

    Create a class

--- student.py
+++ student.py
@@ -52,15 +52,67 @@
 #   main()
 
 """Aproach 4"""
+# def main():
+#   student = get_student()
+#   if student["name"] == "Padma":
+#     student["house"] = "Ravenclaw"
+#   print(f"{student['name']} from {student['house']}")
+
+# # def get_student():
+# #   student = {}
+# #   student["name"] = input("Name: ")
+# #   student["house"] = input("House: ")
+# #   return student
+
+# def get_student():
+#   name = input("Name: ")
+#   house = input("House: ")
+#   return {"name": name, "house": house}
+
+# if __name__ == "__main__":
+#   main()
+
+"""CLASSES"""
+"""docs.python.org/3/tutorial/classes.html"""
+# class Student:
+#   ...
+
+# def main():
+#   student = get_student()
+#   print(f"{student.name} from {student.house}")
+
+# def get_student():
+#   student = Student()
+#   student.name = input("Name: ")
+#   student.house = input("House: ")
+#   return student
+
+# if __name__ == "__main__":
+#   main()
+
+
+class Student:
+  def __init__(self, name, house):
+    if not name:
+      raise ValueError("Missing name")
+    if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+      raise ValueError("Invalid house")
+    self.name = name 
+    self.house = house
+  
+  def __str__(self):
+    return "a student"
+
 def main():
   student = get_student()
-  print(f"{student['name']} from {student['house']}")
+  print(student)
+  # print(f"{student.name} from {student.house}")
 
 def get_student():
-  student = {}
-  student["name"] = input("Name: ")
-  student["house"] = input("House: ")
-  return student
+  name = input("Name: ")
+  house = input("House: ")
+  return Student(name, house)
+
 
 if __name__ == "__main__":
   main()
