a37b63c2ed1ab4f2df1fb63c5be0d22e8f1fd539
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-01 16:43:15 +0000

    Create charm method

--- student.py
+++ student.py
@@ -92,26 +92,41 @@
 
 
 class Student:
-  def __init__(self, name, house):
+  def __init__(self, name, house, patronus):
     if not name:
       raise ValueError("Missing name")
     if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
       raise ValueError("Invalid house")
     self.name = name 
     self.house = house
+    self.patronus = patronus
   
   def __str__(self):
-    return "a student"
+    return f"{self.name} from {self.house}"
+  
+  def charm(self):
+    match self.patronus:
+      case "Stag":
+        return "🐴"
+      case "Otter":
+        return "🦦"
+      case "Jack Russell terrier":
+        return "🐶"
+      case _:
+        return "🪄"
+
 
 def main():
   student = get_student()
-  print(student)
+  print("Expecto Patronum!")
+  print(student.charm())
   # print(f"{student.name} from {student.house}")
 
 def get_student():
   name = input("Name: ")
   house = input("House: ")
-  return Student(name, house)
+  patronus = input("Patronus: ")
+  return Student(name, house, patronus)
 
 
 if __name__ == "__main__":
