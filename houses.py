12764bbd11e03a07546ece69bd592c61aa6afd4c
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-05 10:46:27 +0000

    Using sets

---
+++ houses.py
@@ -0,0 +1,19 @@
+students = [
+  {"name": "Hermione", "house": "Gryfindor"},
+  {"name": "Harry", "house": "Gryfindor"},
+  {"name": "Ron", "house": "Gryfindor"},
+  {"name": "Draco", "house": "Slytherin"},
+  {"name": "Padma", "house": "Ravenclaw"},
+]
+
+# houses = []
+# for student in students:
+#   if student["house"] not in houses:
+#     houses.append(student["house"])
+
+houses = set()
+for student in students:
+  houses.add(student["house"])
+
+for house in sorted(houses):
+  print(house)
