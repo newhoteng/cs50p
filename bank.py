4d38a99eaf00584602510efc8f2a896012c8d2aa
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-05 14:58:51 +0000

    Using oop to encapsulate global variables

--- bank.py
+++ bank.py
@@ -30,3 +30,17 @@ class Account:
   
   def deposit(self, n):
     self._balance += n
+
+  def withdraw(self, n):
+    self._balance -= n
+
+
+def main():
+  account = Account()
+  print("Balance:", account.balance)
+  account.deposit(100)
+  account.withdraw(50)
+  print("Balance:", account.balance)
+
+if __name__ == "__main__":
+  main()
