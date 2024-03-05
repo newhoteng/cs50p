9078c902201c700be0801a8a70f8417ebde3b551
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-05 16:33:19 +0000

    Modify meow function

--- meows.py
+++ meows.py
@@ -33,10 +33,17 @@
 # meow(number)
 
 
-def meow(n: int) -> None:
-  for _ in range(n):
-    print("meow")
+# def meow(n: int) -> None:
+#   for _ in range(n):
+#     print("meow")
+
+# number: int = int(input("Number: "))
+# meows: str = meow(number)
+# print(meows)
+
+def meow(n: int) -> str:
+  return "meow\n" * n
 
 number: int = int(input("Number: "))
 meows: str = meow(number)
-print(meows)
+print(meows, end="")
