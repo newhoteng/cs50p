06d242cb4cff05756502862d824e5aa41f878bb0
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-07 16:23:54 +0000

    Dictionary comprehension

--- gryffindors.py
+++ gryffindors.py
@@ -1,22 +1,37 @@
-students = [
-    {"name": "Hermione", "house": "Gryffindor"},
-    {"name": "Harry", "house": "Gryffindor"},
-    {"name": "Ron", "house": "Gryffindor"},
-    {"name": "Draco", "house": "Slytherin"},
-]
-
-# gryffindors = [
-#   student["name"] for student in students if student["house"] == "Gryffindor"
+# students = [
+#     {"name": "Hermione", "house": "Gryffindor"},
+#     {"name": "Harry", "house": "Gryffindor"},
+#     {"name": "Ron", "house": "Gryffindor"},
+#     {"name": "Draco", "house": "Slytherin"},
 # ]
 
+# # gryffindors = [
+# #   student["name"] for student in students if student["house"] == "Gryffindor"
+# # ]
 
-def is_gryffindor(s):
-    return s["house"] == "Gryffindor"
+
+# def is_gryffindor(s):
+#     return s["house"] == "Gryffindor"
 
 
-# gryffindors = filter(is_gryffindor, students)
+# # gryffindors = filter(is_gryffindor, students)
 
-gryffindors = filter(lambda s: s["house"] == "Gryffindor", students)
+# gryffindors = filter(lambda s: s["house"] == "Gryffindor", students)
 
-for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
-    print(gryffindor["name"])
+# for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+#     print(gryffindor["name"])
+
+"""dictionary comprehension"""
+
+students = ["Hermione", "Harry", "Ron"]
+
+# gryffindors = []
+
+# for student in students:
+#   gryffindors.append({"name": student, "house": "Gryffindor"})
+
+# gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+
+gryffindors = {student: "Gryffindor" for student in students}
+
+print(gryffindors)
