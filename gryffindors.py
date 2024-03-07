73b8b64b0c06c199a86ac4b1280461486e2c77ae
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-07 16:28:41 +0000

    Using enumerate

--- gryffindors.py
+++ gryffindors.py
@@ -23,15 +23,24 @@
 
 """dictionary comprehension"""
 
+# students = ["Hermione", "Harry", "Ron"]
+
+# # gryffindors = []
+
+# # for student in students:
+# #   gryffindors.append({"name": student, "house": "Gryffindor"})
+
+# # gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+
+# gryffindors = {student: "Gryffindor" for student in students}
+
+# print(gryffindors)
+
+
 students = ["Hermione", "Harry", "Ron"]
 
-# gryffindors = []
+# for i in range(len(students)):
+#   print(i + 1, students[i])
 
-# for student in students:
-#   gryffindors.append({"name": student, "house": "Gryffindor"})
-
-# gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
-
-gryffindors = {student: "Gryffindor" for student in students}
-
-print(gryffindors)
+for i, student in enumerate(students):
+  print(i + 1, student)
