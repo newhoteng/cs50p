d954e7805d5095176827d12d2a4ca90888c8c923
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-07 15:59:56 +0000

    Filter function

---
+++ gryffindors.py
@@ -0,0 +1,18 @@
+students = [
+  {"name": "Hermione", "house": "Gryffindor"},
+  {"name": "Harry", "house": "Gryffindor"},
+  {"name": "Ron", "house": "Gryffindor"},
+  {"name": "Draco", "house": "Slytherin"},
+]
+
+# gryffindors = [
+#   student["name"] for student in students if student["house"] == "Gryffindor"
+# ]
+
+def is_gryffindor(s):
+  return s["house"] == "Gryffindor"
+
+gryffindors = filter(is_gryffindor, students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+  print(gryffindor["name"])
--- houses.py
+++ houses.py
@@ -1,7 +1,7 @@
 students = [
-  {"name": "Hermione", "house": "Gryfindor"},
-  {"name": "Harry", "house": "Gryfindor"},
-  {"name": "Ron", "house": "Gryfindor"},
+  {"name": "Hermione", "house": "Gryffindor"},
+  {"name": "Harry", "house": "Gryffindor"},
+  {"name": "Ron", "house": "Gryffindor"},
   {"name": "Draco", "house": "Slytherin"},
   {"name": "Padma", "house": "Ravenclaw"},
 ]
