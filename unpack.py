fec3056429fa688b5a06b8876494d9010c4e6aa9
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-07 14:00:29 +0000

    Variable number of arguments and key word arguments

--- unpack.py
+++ unpack.py
@@ -5,9 +5,26 @@ Unpacking
 # first, _ = input("What's your name? ").split(" ")
 # print(f"hello, {first}")
 
-def total(galleons, sickles, knuts):
-  return (galleons * 17 + sickles) * 29 + knuts
+# def total(galleons, sickles, knuts):
+#   return (galleons * 17 + sickles) * 29 + knuts
 
-coins = [100, 50, 25]
+# coins = [100, 50, 25]
 
-print(total(*coins), "Knuts")
+# print(total(*coins), "Knuts")
+
+
+# def total(galleons, sickles, knuts):
+#   return (galleons * 17 + sickles) * 29 + knuts
+
+# coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+# # print(total(coins["galleons"], coins["sickles"], coins["knuts"]), "Knuts")
+# # print(total(galleons=100, sickles=50, knuts=25), "Knuts")
+# print(total(**coins))
+
+def f(*args, **kwargs):
+  print("Positional:", args)
+  print("Named:", kwargs)
+
+f(100, 50, 25, 5, galleons=100)
+
