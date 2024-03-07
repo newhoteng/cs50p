812292831bda3a2ae648ad72e217e0bfa5f2abdb
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-07 16:12:56 +0000

    Using black to format python code

--- gryffindors.py
+++ gryffindors.py
@@ -1,18 +1,22 @@
 students = [
-  {"name": "Hermione", "house": "Gryffindor"},
-  {"name": "Harry", "house": "Gryffindor"},
-  {"name": "Ron", "house": "Gryffindor"},
-  {"name": "Draco", "house": "Slytherin"},
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
 ]
 
 # gryffindors = [
 #   student["name"] for student in students if student["house"] == "Gryffindor"
 # ]
 
-def is_gryffindor(s):
-  return s["house"] == "Gryffindor"
 
-gryffindors = filter(is_gryffindor, students)
+def is_gryffindor(s):
+    return s["house"] == "Gryffindor"
+
+
+# gryffindors = filter(is_gryffindor, students)
+
+gryffindors = filter(lambda s: s["house"] == "Gryffindor", students)
 
 for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
-  print(gryffindor["name"])
+    print(gryffindor["name"])
