7652d31dffa41822d3b344b52f621d581b57d61c
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-07 09:50:21 +0000

    Argparse library

--- meows.py
+++ meows.py
@@ -54,20 +54,47 @@ peps.python.org/pep-0257/
 standardizes how you should document your code.
 """
 
-def meow(n: int) -> str:
-  """Meow n times."""
-  
-  """
-  Meow n times.
+# def meow(n: int) -> str:
+#   """Meow n times."""
 
-  :param n: Number of times to meow
-  :type n: int
-  :raise TypeError: If n is not an int
-  :returns: A string of n meows, one per line
-  :rtype: str
-  """
-  return "meow\n" * n
+#   """
+#   Meow n times.
 
-number: int = int(input("Number: "))
-meows: str = meow(number)
-print(meows, end="")
+#   :param n: Number of times to meow
+#   :type n: int
+#   :raise TypeError: If n is not an int
+#   :returns: A string of n meows, one per line
+#   :rtype: str
+#   """
+#   return "meow\n" * n
+
+# number: int = int(input("Number: "))
+# meows: str = meow(number)
+# print(meows, end="")
+
+# import sys
+
+# if len(sys.argv) == 1:
+#   print("meow")
+# elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+#   n = int(sys.argv[2])
+#   for _ in range(n):
+#     print("meow")
+# else:
+#   print("usage: meows.py")
+
+"""
+argparse
+docs.python.org/3/library/argparse.html
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", default=1, help="number of times to meow", type=int)
+args = parser.parse_args()
+
+# print(args)
+
+for _ in range(args.n):
+  print("meow")
