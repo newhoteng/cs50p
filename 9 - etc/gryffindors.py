74cb049f6fdb3eea926f82f83feba51b497381c3
Harriet Oteng <harriet.oteng@gmail.com>
2024-03-27 09:43:04 +0000

    Reverse string

--- 9 - etc/gryffindors.py
+++ 9 - etc/gryffindors.py
@@ -44,3 +44,4 @@ students = ["Hermione", "Harry", "Ron"]
 
 for i, student in enumerate(students):
   print(i + 1, student)
+
--- self.py
+++ self.py
@@ -16,17 +16,41 @@
 # print(LCM(3, 7))
 
 
-list = [0, 1, 2, 3, 4, 5, 6]
+# list = [0, 1, 2, 3, 4, 5, 6]
 
-print(list.pop())
-print(list)
-print(list.pop())
-print(list.pop(0))
-print(list.pop(0))
+# print(list.pop())
+# print(list)
+# print(list.pop())
+# print(list.pop(0))
+# print(list.pop(0))
 
+# def gcdOfStrings(str1: str, str2: str) -> str:
+#     # length = min(len(str1), len(str2))
+#     length = len(str1) if len(str1) <= len(str2) else len(str2)
 
+#     if str1 + str2 != str2 + str1:
+#         return ""
 
+#     for i in range(length, 0, -1):
+#         if len(str1) % i == 0 and len(str2) % i == 0:
+#             return str1[0: i]
 
+# print(gcdOfStrings("abcabc", "abc"))
+
+# def canPlaceFlowers(flowerbed, n: int) -> bool:
+#     for i in range(len(flowerbed)):
+#         if flowerbed[i-1] == 0 and flowerbed[i+1]:
+#             n - 1
+#             i + 1
+
+# s = "a good   example"
+s = "hello world"
+print(s[::-1])
+# s = s.split()
+# print(s)
+
+# for word in s:
+#   print(word)
 
 
 
