341965c4fe32c95aa0ca488e3791f01ec63a70c2
Harriet Oteng <harriet.oteng@gmail.com>
2024-05-12 21:53:40 +0000

    Leetcode practice

--- self.py
+++ self.py
@@ -1,68 +1,28 @@
 from functools import reduce
 
-# print(int(7/2))
-# print(int(2/3))
-
-# for i in range(6,0,-1):
-#   print(i)
-
-
-# def LCM(a, b):
-#   max = a if a >= b else b
-
-#   while True:
-#     if (max % a == 0 and max % b == 0):
-#       return max
-#     max += 1
-
-# print(LCM(3, 7))
-
-
-# list = [0, 1, 2, 3, 4, 5, 6]
-
-# print(list.pop())
-# print(list)
-# print(list.pop())
-# print(list.pop(0))
-# print(list.pop(0))
-
-# def gcdOfStrings(str1: str, str2: str) -> str:
-#     # length = min(len(str1), len(str2))
-#     length = len(str1) if len(str1) <= len(str2) else len(str2)
-
-#     if str1 + str2 != str2 + str1:
-#         return ""
-
-#     for i in range(length, 0, -1):
-#         if len(str1) % i == 0 and len(str2) % i == 0:
-#             return str1[0: i]
-
-# print(gcdOfStrings("abcabc", "abc"))
-
-# def canPlaceFlowers(flowerbed, n: int) -> bool:
-#     for i in range(len(flowerbed)):
-#         if flowerbed[i-1] == 0 and flowerbed[i+1]:
-#             n - 1
-#             i + 1
-
-# s = "a good   example"
-# s = "hello world"
-# print(s[:])
-# s = s.split()
-# print(s)
-
-# for word in s:
-#   print(word)
-
-lst = [1, 2, 3, 1, -1, 1]
-ans = []
-for i in range(len(lst)):
-    hold = lst[i]
-    lst[i] = 1
-    # p = 1
-    # for j in range(len(lst)):
-    #     p = p * lst[j]
-    # ans.append(p)
-    ans.append(reduce(lambda x, y: x * y, lst))
-    lst[i] = hold
-print(ans)
+def maxOperations(nums, k):
+    # Initialize a hash map to count occurrences of each number
+    count = {}
+    # Initialize operations counter
+    operations = 0
+    
+    # Iterate through each number in the array
+    for num in nums:
+        # Calculate the complement
+        complement = k - num
+        
+        # If the complement is present in the hash map and has a count greater than zero
+        if complement in count and count[complement] > 0:
+            # Perform an operation
+            operations += 1
+            # Decrease the count of the complement in the hash map
+            count[complement] -= 1
+        else:
+            # Add the current number to the hash map or increment its count if it already exists
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+    
+    # Return the total number of operations performed
+    return operations
